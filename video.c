8e969d3be9806e0dab388094f5efe3030777c16b
M. Alvarez Josue <j_alvare@ibm.insa-toulouse.fr>
2016-04-05 09:56:46 +0200

    debut de acquireImage

--- video.c
+++ video.c
@@ -13,5 +13,12 @@ void computePosition(void* arg)
 
 void acquireImage(void* arg)
 {
-    
+    rt_task_set_periodic(NULL, TM_NOW, 600e+6);
+
+    while (1) {
+        /* Attente de l'activation périodique */
+        rt_task_wait_period(NULL);
+        rt_printf("tImageAcquisition : Activation périodique\n");
+    }
+
 }
